a2ff6de57df80f083d0c87296911e4abe6749c40
AcheronX0577 <richardhuynh53@gmail.com>
2025-09-20 14:28:19 -0700

    change order of import

--- script/amazon.js
+++ script/amazon.js
@@ -1,4 +1,4 @@
-import {addToCart, cart} from '../data/cart.js'
+import {cart, addToCart} from '../data/cart.js'
 import { products } from '../data/products.js';
 
 
@@ -65,8 +65,8 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 function updateCartQuantity(){
     let cartQuantity = 0;
 
-    cart.forEach((item) => {
-        cartQuantity += item.quantity;
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
     });
 
     document.querySelector('.js-cart-quantity')
